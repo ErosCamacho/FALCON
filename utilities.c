e6c9f733dabc8ad815ecbba6b9a0633bedeaaec4
ErosCamacho <eroscamaru@gmail.com>
2022-04-22 14:42:47 +0200

    Add files via upload

--- utilities.c
+++ utilities.c
@@ -2,6 +2,6 @@
 
 void printchar(char* name, unsigned char* A, unsigned long long L) {
 	printf("%s: ", name);
-	for (i = 0; i < L; i++) printf("%02X", A[i]);
+	for (int i = 0; i < L; i++) printf("%02X", A[i]);
 	printf("\n");
 }
