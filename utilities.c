0054e3f5d6417697fb7138cd53f0f2124c4e7d9d
ErosCamacho <eroscamaru@gmail.com>
2022-04-22 14:41:51 +0200

    Add files via upload

--- utilities.c
+++ utilities.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-#include<conio.h>
 
 void printchar(char* name, unsigned char* A, unsigned long long L) {
 	printf("%s: ", name);
--- utilities.h
+++ utilities.h
@@ -1,4 +1,3 @@
 #include<stdio.h>
-#include<conio.h>
 
 void printchar(char* name, unsigned char* A, unsigned long long L);
